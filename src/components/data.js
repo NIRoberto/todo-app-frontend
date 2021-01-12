cc60c0ec4b87a3ec90bb0f37802c0c3e6d68c58b
Robert NIYITANGA <robertwilly668@gmail.com>
2021-01-12 09:39:15 +0200

    *Setup webpack configuration

---
+++ src/components/data.js
@@ -0,0 +1,18 @@
+export const wrestles = [
+   {
+      id: 1,
+      name: ' Roman Reigns ',
+   },
+   {
+      id: 2,
+      name: ' Seth Rollins ',
+   },
+   {
+      id: 3,
+      name: 'Block ',
+   },
+   {
+      id: 4,
+      name: 'Under taker',
+   },
+];
